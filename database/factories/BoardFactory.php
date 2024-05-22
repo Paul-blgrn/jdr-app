20fc86118c3ffc16c3f2eea804686b4ab0ce4b9f
Paul-blgrn <paul.blgrn@gmail.com>
2024-05-22 19:35:40 +0200

    🚧 breeze installation

---
+++ database/factories/BoardFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Board>
+ */
+class BoardFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->words(3, true),
+            'description' => $this->faker->sentence(),
+            'capacity' => $this->faker->numberBetween(2,10),
+            'invitation_code' => $this->faker->word(),
+        ];
+    }
+}
